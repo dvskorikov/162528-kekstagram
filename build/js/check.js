9d6b58099ace61e8404751391612991535729e61
Dmitry Skorikov <dvskorikov@mail.ru>
2016-03-26 13:44:33 +0300

    функция getMessage

---
+++ build/js/check.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function getMessage(a, b) {
+  // GIF
+  if (typeof a === "boolean") {
+    // GIF анимированный
+    if (a) {
+      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      // GIF НЕанимированный
+    } else {
+      return "Переданное GIF-изображение не анимировано";
+    }
+    // SVG
+  } else if (typeof a === "number") {
+    return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
+    // JPG
+  } else if ((typeof a === "object") && (typeof b === "object")) {
+    var twoArraysAmount = 0;
+    a.forEach(function(elem, i, arr) {
+      twoArraysAmount += a[i] * b[i];
+    });
+    return "Общая площадь артефактов сжатия: " + twoArraysAmount + " пикселей";
+    // PNG
+  } else if (typeof a === "object") {
+    var arrayAmount = a.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    return "Количество красных точек во всех строчках изображения: " + arrayAmount;
+  }
+}
